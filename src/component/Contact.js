06a8d58e84392cc28a9053b4b489cf83dc804397
JQ <xujacque@gmail.com>
2022-11-02 14:55:39 -0400

    context added to about&contact component + styled part of about&contact

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -2,8 +2,27 @@ import React from "react"
 
 export default function Contact() {
     return (
-        <div>
-            
-        </div>
+        <form>
+            <fieldset>
+                <legend>Contact us</legend>
+                <p className="contact-headline">Got any questions, suggestions, or concerns? Feel free to let us know!</p>
+                    <div className="contact-container">
+                    <label for="first-name">Full Name:</label>
+                        <input type="text" id="full-name" 
+                        placeholder="Joe Smith"></input>
+                    <label for="email">Email:</label>
+                        <input type="email" id="email" 
+                        placeholder="joes1@gmail.com"></input>  
+                    <label for="phonenumber">Phone Number:</label>
+                        <input type="tel" id="telephone" pattern="[0-9]{3}-[0-9]{3}- [0-9]{4}" 
+                        placeholder="123-456-7890"></input> 
+                    <label for="message"> Message: </label>
+                        <textarea id="message" rows="5"></textarea>
+                </div>
+                    <div className="submit-button">
+                    <button className="contact-button" type="submit">Submit</button>
+                    </div>
+            </fieldset>
+        </form>
     )
 }
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function NavBar() {
     return (
-        <div className="NavBar-container">
+        <div className="navBar-container">
             <ul>
                 <li>About</li>
                 <li>Product</li>
