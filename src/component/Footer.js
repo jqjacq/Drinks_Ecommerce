3cf627718f276e3864d4fec5e7c542fb99032dca
JQ <xujacque@gmail.com>
2022-11-04 11:12:37 -0400

    added more info to footer: social media and additional inquiries

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,10 +2,30 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <footer>
+        <footer className="footer">
+            <br></br>
+            <p className="socialmedia">
+                <i class="fa-brands fa-lg fa-github"></i>
+                <i class="fa-brands fa-lg fa-facebook"></i>
+                <i class="fa-brands fa-lg fa-instagram"></i>
+                <i class="fa-brands fa-lg fa-twitter"></i>
+                <i class="fa-brands fa-lg fa-youtube"></i>
+            </p>
+            <p className="additionalinfo">
+                <ul>
+                    <li>About us</li>
+                    <li>Shipping Status</li>
+                    <li>Return Policy</li>
+                    <li>Feedback</li>
+                    <li>Location</li>
+                    <li>Contact Us</li>
+                </ul>
+            </p>
             <hr></hr>
-            <span className="copyright">COPYRIGHT © JQ Tea and Coffee Co., All rights reserved</span> 
-            <span className="notForSale">· THIS IS NOT AN ACTUAL PRODUCT FOR SALE.</span> 
+            <div className="copyright">
+                <span>COPYRIGHT © JQ Tea and Coffee Co., All rights reserved</span> 
+                <span>· THIS IS NOT AN ACTUAL PRODUCT FOR SALE.</span> 
+            </div>
         </footer>
     )
 }
--- src/component/Product.js
+++ src/component/Product.js
@@ -1,14 +1,14 @@
 import React from "react"
 
-export default function Product(props) {
+export default function Product(props) {    
     return (
         <figure>
-        <img src = {props.img} className="coffeeteaImage"
-        alt={`${props.type}`}/>
-        <p className = "coffeeteaType"> {props.type} </p>
-        <p>{props.pricePerLb}
+            <img src = {props.img} className="coffeeteaImage"
+            alt={`${props.type}`}/>
+            <p className = "coffeeteaType"> {props.type} </p>
+            <p>{props.pricePerLb}
             <button>BUY</button> 
-        </p>
+            </p>
         </figure>
     )
     }
