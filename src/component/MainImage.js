2a47aebd3d0df66b487afce9d57cff63647466a6
JQ <xujacque@gmail.com>
2022-11-11 16:35:09 -0500

    changed logo

--- src/component/MainImage.js
+++ src/component/MainImage.js
@@ -3,11 +3,12 @@ import React from "react"
 export default function MainImage() {
     return (
     <div className="mainimage-container">
-        <img src="../img/front.jpg" className="mainImage" alt="field of greens"/>
-            <span className="logo">
-                    <i className="fa-solid fa-leaf fa-lg"></i>
-                    JQ's Tea and Coffee Store 
-            </span>
+        <img src="../img/adjustedfront.png" className="mainImage" alt="field of greens"/>
+                <img src = "../img/jqlogo.png" className="logo" alt="JQ Logo" />
+            {/* <span className="logo">  
+                <i className="fa-solid fa-leaf fa-lg"></i>
+                JQ's Tea and Coffee Store
+            </span> */}
     </div> 
     )
 }
