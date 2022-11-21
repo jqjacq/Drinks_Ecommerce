af5e47fcdefc047b9f9c5ae0bc196388622a053e
JQ <xujacque@gmail.com>
2022-11-21 18:04:03 -0500

    changed code for img changer since it was glitching

--- src/component/ImageChanger.js
+++ src/component/ImageChanger.js
@@ -1,49 +1,33 @@
-import React from "react"
+import React, { useEffect, useState, useMemo } from "react"
 import "../index.css";
 
-class ImageChanger extends React.Component {
-    constructor(props) {
-        super(props);
-        this.switchImage = this.switchImage.bind(this);
-        this.state = {
-          currentImage: 0,
-          images: [
-            "/img/maincovers/adjustedfront.png",
-            "/img/maincovers/adjustedtea.png",
-            "/img/maincovers/adjustedcoffee2.png"
-          ]
-        };
-      }
-    
-    switchImage() {
-        if (this.state.currentImage < this.state.images.length - 1) {
-          this.setState({
-            currentImage: this.state.currentImage + 1
-          });
-        } else {
-          this.setState({
-            currentImage: 0
-          });
-        }
-        return this.currentImage;
-      }
-    
-    timeImage() {
-        setInterval(this.switchImage, 4000);
-      }
+export default function ImageChange() {
+  const cacheImage = useMemo(() => 
+    [
+      "/img/maincovers/adjustedfront.png",
+      "/img/maincovers/adjustedtea.png",
+      "/img/maincovers/adjustedcoffee2.png"
+  ], [])
+  const [currentImage, setCurrentImage] = useState(0)
+  const intervalTime = 4000
 
-    render() {
-        this.timeImage()
-        return (
-        <div className="mainimage-container">
-          <img
-            src={this.state.images[this.state.currentImage]}
-            alt="changing cover of field of green, tea and coffee"
-          />
-          <img src = "../img/jqlogo.png" className="logo" alt="JQ Logo" />
-        </div>
-        )
-    }
+  function setTimer() {
+    let nextImage = currentImage + 1
+      if (nextImage >= cacheImage.length) {
+        nextImage = 0
+      }
+    setTimeout(() => setCurrentImage(nextImage), intervalTime)
+  }
+
+  useEffect(setTimer, [cacheImage, currentImage])
+
+  return (
+      <div className="mainimage-container">
+        <img
+          src={cacheImage[currentImage]}
+          alt="changing cover of field of green, tea and coffee"
+        />
+        <img src = "../img/jqlogo.png" className="logo" alt="JQ Logo" />
+      </div>
+  )
 }
-
-export default ImageChanger
