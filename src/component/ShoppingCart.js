43af11624396d5e0223cf4385c3c0fe7036d92d8
Jacqueline Xu <85534903+jqjacq@users.noreply.github.com>
2022-12-01 18:30:43 -0500

    order summary adds up total

--- src/component/ShoppingCart.js
+++ src/component/ShoppingCart.js
@@ -3,26 +3,34 @@ import InCart from "./InCart"
 
 export default function Cart(props) {
     const { cartItems, onAdd, onRemove } = props
+    let subTotal = 0.00
+    for  (let i=0; i<cartItems.length; i++) {
+        subTotal += parseFloat(cartItems[i].weight) * cartItems[i].quantity
+    }
+    let discountPrice = 0.00
+    let shippingPrice = subTotal > 99 ? 0.00 : 8.99
+    let totalPrice = parseFloat(subTotal + discountPrice + shippingPrice)
 
     return (
         <>
             <header><h1 className="centertext"> Shopping Cart</h1></header>
             <div className="row"> 
                 <InCart onAdd={onAdd} onRemove={onRemove} cartItems={cartItems}></InCart>
+                
                 <aside className="col-1">
                     <h3 className="cart-section">Order Summary</h3>
                     <p> Subtotal: 
-                        <span className="calculation">$0.00</span>
+                        <span className="calculation">${ subTotal.toFixed(2) }</span>
                     </p>
                     <p> Discount:
-                        <span className="calculation">$0.00</span>
+                        <span className="calculation">${discountPrice}</span>
                     </p>
-                    <p> Shipping & Handling: 
-                        <span className="calculation">$0.00</span>
+                    <p> Shipping: 
+                        <span className="calculation">${shippingPrice}</span>
                     </p>
                     <hr></hr>
                     <p className="bold"> Total 
-                        <span className="calculation">$0.00</span>
+                        <span className="calculation">${totalPrice.toFixed(2)}</span>
                     </p>
                     <hr></hr>
                     <button> Checkout </button>
